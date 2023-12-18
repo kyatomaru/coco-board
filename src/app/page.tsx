41cd00d252f089704aa965398e9f04987b508a96
cark <cark101060@gmail.com>
2023-12-18 20:59:25 +0900

    エラーを修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 "use client"
 
+import * as React from 'react';
 import TitleBox from "@/components/TitleBox";
 import LoginBox from "@/components/auths/LoginBox";
 import dayjs from 'dayjs';
 import { useRouter } from 'next/navigation'
 
 export default function Home() {
-  const router = useRouter()
-  router.replace('/notes/' + (dayjs(String(new Date())).format('YYYY-MM-DD')));
+  React.useEffect(() => {
+    const router = useRouter()
+    router.replace('/notes/' + (dayjs(String(new Date())).format('YYYY-MM-DD')));
+  }, [])
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
