3acc8e2f8b2e6d8b45e21c7a893fe6c0544bc089
cark <cark101060@gmail.com>
2024-06-25 20:55:43 +0900

    日本語に設定

--- src/app/create/[date]/board/layout.tsx
+++ src/app/create/[date]/board/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="ja">
       <body style={bodyStyle} className={inter.className}>{children}</body>
     </html >
   )
