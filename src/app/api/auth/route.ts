547fef10fd82e9c5237b69267af97eaaf56076b1
cark <cark101060@gmail.com>
2024-06-28 13:31:19 +0900

    fix: メール認証エラーの修正

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -46,7 +46,8 @@ export async function GET(
             // Display email verification handler and UI.
             // return NextResponse.redirect(`/accounts/verifyemail/${actionCode}`)
             if (handleVerifyEmail(auth, actionCode, continueUrl, lang))
-                return NextResponse.redirect(new URL(`/accounts/verifyemail/${actionCode}`, req.url))
+                // return NextResponse.redirect(new URL(`/accounts/verifyemail/${actionCode}`, req.url))
+                return NextResponse.redirect(new URL(`/home`, req.url))
             else
                 return NextResponse.redirect(new URL(`/accounts/verifyemail/error`, req.url))
         default:
@@ -92,11 +93,11 @@ function handleResetPassword(auth, actionCode, continueUrl, lang) {
 }
 
 
-function handleVerifyEmail(auth, actionCode, continueUrl, lang) {
+const handleVerifyEmail = async (auth, actionCode, continueUrl, lang) => {
     // Localize the UI to the selected language as determined by the lang
     // parameter.
     // Try to apply the email verification code.
-    applyActionCode(auth, actionCode).then((resp) => {
+    const res = await applyActionCode(auth, actionCode).then((resp) => {
         // Email address has been verified.
 
         // TODO: Display a confirmation message to the user.
@@ -109,7 +110,10 @@ function handleVerifyEmail(auth, actionCode, continueUrl, lang) {
     }).catch((error) => {
         // Code is invalid or expired. Ask the user to verify their email address
         // again.
-
+        return false
     });
-    return false
+
+    console.log(res)
+
+    return res
 }
