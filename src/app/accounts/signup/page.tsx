72fa8686371903d6932e994fce92f342a8fc4769
ko610 <83527519+ko610@users.noreply.github.com>
2024-09-06 19:10:58 +0900

    Update page.tsx

--- src/app/accounts/signup/page.tsx
+++ src/app/accounts/signup/page.tsx
@@ -22,7 +22,6 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between bg-white">
-      あいう
       <SignupPage />
     </main >
   )
