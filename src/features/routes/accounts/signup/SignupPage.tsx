9515c7cf13c2afadc3eee6cc7b488efd0ef25be3
ko610 <83527519+ko610@users.noreply.github.com>
2024-09-06 19:08:58 +0900

    Update SignupPage.tsx

--- src/features/routes/accounts/signup/SignupPage.tsx
+++ src/features/routes/accounts/signup/SignupPage.tsx
@@ -25,10 +25,6 @@ export default function SignupPage() {
     const [user, setUser] = React.useState<User | undefined>()
 
     return (
-        <>
-            {!isLoading ?
-                <LoadingPage />
-                :
                 <Container maxWidth="xs" fixed sx={{ mt: { xs: "30px", md: "70px" }, mb: "30px" }}>
                     <Box sx={{ px: "20px", pt: "30px", pb: "15px", textAlign: "center", border: "solid 0.5px #b2b2b2" }}>
                         <Stack
@@ -75,7 +71,5 @@ export default function SignupPage() {
                         </Typography>
                     </Box>
                 </Container>
-            }
-        </>
     );
 }
