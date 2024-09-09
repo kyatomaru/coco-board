9d5bb6c104a020c4d1a1417079ddea3ca8d6c6b4
cark <cark101060@gmail.com>
2024-09-09 21:16:06 +0900

    fix:エラー修正

--- src/hooks/common/useSwipeLock.ts
+++ src/hooks/common/useSwipeLock.ts
@@ -1,13 +1,16 @@
 import React, { useEffect, useCallback } from "react";
 
 export const useSwipeLock = () => {
+    let sw = false
+
     /**
      * イベントリスナーの設定
      */
     useEffect(() => {
         // モバイルスクロール禁止処理
-        document.addEventListener("touchstart", (e) => { if (e.touches[0].clientX < 30 || e.touches[0].clientX > window.innerWidth - 30) e.preventDefault() }, false);
-        document.addEventListener("touchmove", (e) => { if (e.touches[0].clientX < 30 || e.touches[0].clientX > window.innerWidth - 30) e.preventDefault() }, false);
+        document.addEventListener("touchstart", (e) => { if (e.touches[0].clientX < 30 || e.touches[0].clientX > window.innerWidth - 30) sw = true }, false);
+        document.addEventListener("touchmove", (e) => { if (sw) (e.preventDefault(), alert('test')) }, false);
+        document.addEventListener("touchend", (e) => { sw = false }, false);
 
     }, []);
 
