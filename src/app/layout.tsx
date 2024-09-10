3ddda9580b05d135f4937138c25c0ac146cd6afa
cark <cark101060@gmail.com>
2024-09-10 10:19:25 +0900

    fix:googleアナリティクスタグをlayoutに直接記入

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata, Viewport } from 'next'
 import { Inter, Roboto_Mono } from 'next/font/google'
 import '@/app/globals.css'
-import GoogleAnalytics from '@/components/GoogleAnalytics/GoogleAnalytics'
+import { GoogleAnalytics } from '@next/third-parties/google'
+import { GA_TAG_ID } from '@/libs/gtag'
 
 const inter = Inter({ subsets: ['latin'] })
 const robot_mono = Roboto_Mono({ subsets: ['latin'] })
@@ -25,10 +26,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
-      <head>
-        <GoogleAnalytics />
-      </head>
-      <body className={robot_mono.className}> {children}</body>
+      <GoogleAnalytics gaId={`${GA_TAG_ID}`} />
+      <body className={robot_mono.className}>{GA_TAG_ID}{children}</body>
     </html >
   )
 }
