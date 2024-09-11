1a0f9f6f155961bd98ec64cf5b9dbbd9c83842ab
cark <cark101060@gmail.com>
2024-09-11 21:22:53 +0900

    fix:エラー修正

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -51,16 +51,16 @@ export default function Home() {
           {/* <Header /> */}
           <LeftBar />
           <HomeHeader date={date} setDate={setDate} displayMenu={displayMenu} setDisplayMenu={setDisplayMenu} />
-          <Container id="test" maxWidth="md" sx={{ position: "relative", overflowY: "auto", height: "100vh", mx: 0, mt: { xs: "146px", md: "110px" }, px: 0, pb: "250px", pl: { md: "120px", lg: "250px" } }}>
+          <Container id="test" maxWidth="md" sx={{ position: "relative", overflowY: "auto", height: "100vh", mx: 0, mt: { xs: "146px", md: "110px" }, px: 0, pl: { md: "120px", lg: "250px" } }}>
 
             {displayMenu == 0 &&
-              <Box sx={{ mt: 2, borderRadius: 2, px: 2 }}>
+              <Box sx={{ mt: 2, pb: "250px", borderRadius: 2, px: 2 }}>
                 <CreateButton onClick={() => { window.scrollTo(0, 0); setIsNoteCreateModal(0) }} />
                 <BoardCardBox user={user} contents={board} setContents={setBoard} date={date} menu={isNoteCreateModal} setMenu={setIsNoteCreateModal} />
               </Box>
             }
             {displayMenu == 1 &&
-              <Box sx={{ mt: 2, borderRadius: 2, px: 2 }}>
+              <Box sx={{ mt: 2, pb: "250px", borderRadius: 2, px: 2 }}>
                 <CreateButton onClick={() => { setIsNoteCreateModal(1) }} />
                 <NoteCardBox user={user} contents={note} setContents={setNote} date={date} menu={isNoteCreateModal} setMenu={setIsNoteCreateModal} />
               </Box>
