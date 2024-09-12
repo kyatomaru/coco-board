0fa1b9e2246af2e117d7733755cbde431db319df
cark <cark101060@gmail.com>
2024-09-12 11:29:15 +0900

    change:homeページいろいろ変更

--- src/features/common/button/CreateButton.tsx
+++ src/features/common/button/CreateButton.tsx
@@ -12,9 +12,9 @@ export default function CreateButton({ onClick }: PageProps) {
     const router = useRouter()
 
     return (
-        <Box sx={{ position: 'fixed', right: 0, left: 0, height: "-webkit-fill-available", zIndex: 1050, pointerEvents: "none" }} >
+        <Box sx={{ position: 'fixed', top: 0, right: 0, left: 0, height: "-webkit-fill-available", zIndex: 1050, pointerEvents: "none" }} >
             <Container maxWidth="md" sx={{ my: 0, height: "100vh", px: 0, position: "relative", pl: { md: "120px", lg: "250px" } }}>
-                <Fab sx={{ bottom: 300, position: "absolute", pointerEvents: "auto", right: 30, backgroundColor: "#2e7d32 !important" }} color="primary" aria-label="add"
+                <Fab sx={{ bottom: { md: 200, xs: "25vh" }, position: "absolute", pointerEvents: "auto", right: 30, backgroundColor: "#2e7d32 !important" }} color="primary" aria-label="add"
                     onClick={(event) => { onClick() }}>
                     <AddIcon />
                 </Fab>
