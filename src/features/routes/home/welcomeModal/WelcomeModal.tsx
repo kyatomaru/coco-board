c8a7ef58fdd191fe3d9e1be642d372e90ecf56aa
cark <cark101060@gmail.com>
2024-09-13 23:03:32 +0900

    fix:エラー修正

--- src/features/routes/home/welcomeModal/WelcomeModal.tsx
+++ src/features/routes/home/welcomeModal/WelcomeModal.tsx
@@ -57,7 +57,7 @@ export default function WelcomeModal() {
                 </Paper>
             </Modal>
 
-            {page == 3 &&
+            {open && page == 3 &&
                 <Box sx={{ position: 'fixed', top: 0, right: 0, left: 0, height: "-webkit-fill-available", zIndex: 2000, pointerEvents: "none" }} >
                     <Container maxWidth="md" sx={{ my: 0, height: "100vh", px: 0, position: "relative", pl: { md: "120px", lg: "250px" } }}>
                         <Fab sx={{ bottom: { md: 200, xs: "25vh" }, position: "absolute", pointerEvents: "auto", right: 30, backgroundColor: "#2e7d32 !important" }} color="primary" aria-label="add">
