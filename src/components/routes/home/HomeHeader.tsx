3858aab6c9d494f93b8bd8fa2e9eba258e6e7782
cark <souryu1010@gmail.com>
2024-10-18 11:01:52 +0900

    change:日付のデザイン変更

--- src/components/routes/home/HomeHeader.tsx
+++ src/components/routes/home/HomeHeader.tsx
@@ -84,7 +84,7 @@ export default function HomeHeader({ date, setDate, displayMenu, setDisplayMenu,
                     <IconButton href={`/calendar/${dayjs(String(date)).format('YYYY-MM-DD')}`} sx={{ mr: 1 }}>
                         <EventIcon />
                     </IconButton>
-                    <Typography component="h2" fontSize={17} letterSpacing={1} fontWeight={500} sx={{ mt: "2px" }}>
+                    <Typography component="h2" fontSize={17} fontWeight={500} sx={{ mt: "2px" }}>
                         {useDateFormat(String(date))}
                     </Typography>
                 </Stack>
