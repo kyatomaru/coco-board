a666074c3211b54bfe80dceb9d801cf1ba8ecec0
kyatomaru <souryu1010@gmail.com>
2025-06-30 16:06:13 +0900

    fix: lintエラー修正

--- src/features/routes/board/CreateBoardFormBox.tsx
+++ src/features/routes/board/CreateBoardFormBox.tsx
@@ -4,18 +4,10 @@ import * as React from 'react';
 import { useParams, useRouter } from 'next/navigation'
 import { useIsAuth } from '@/hooks/auth/useIsAuth';
 import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
-import MenuSelectBox from '@/features/common/create/MenuSelectBox';
-import type { User } from 'firebase/auth';
-import { auth } from '@/app/firebase';
-import { onAuthStateChanged, getAuth } from "firebase/auth"
-import LoginPage from '@/features/routes/accounts/login/LoginPage';
 import BoardViewForm from '@/features/common/forms/board/BoardViewForm';
 import { BoardModel } from '@/types/board/Board';
 import { useInsertBoard } from '@/hooks/board/useInsertBoard';
-import GameForm from '@/features/common/forms/game/GameForm';
 import dayjs from 'dayjs';
-import LoadingPage from '@/components/LoadingPage';
 
 type PageProps = {
     allContents: Array<any>,
